cd3a9e4fada97d62bca204db0f231f5af63c19e9
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-18 18:36:04 -0500

    updated file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { DocumentsPageComponent } from './documents-page/documents-page.component';
-import { AuthPageComponent } from './auth-page/auth-page.component';
 import { LoadingComponent } from './shared/loading/loading.component';
 import { ErrorComponent } from './shared/error/error.component';
 import { MortgageCalculatorComponent } from './components/mortgage-calculator/mortgage-calculator.component';
 import { DocumentViewerComponent } from './components/document-viewer/document-viewer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { DocumentsComponent } from './documents/documents.component';
 import { LandingComponent } from './landing/landing.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LandingPageComponent,
-    DocumentsPageComponent,
-    AuthPageComponent,
     LoadingComponent,
     ErrorComponent,
     MortgageCalculatorComponent,
     DocumentViewerComponent,
     HeaderComponent,
+    LandingComponent,
     DocumentsComponent,
-    LandingComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule,
