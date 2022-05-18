db1d15561d3ecf7631f5464471b4c6963ece719e
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-18 18:27:59 -0500

    updated file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DocumentsPageComponent } from './documents-page/documents-page.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { LoadingComponent } from './shared/loading/loading.component';
+import { ErrorComponent } from './shared/error/error.component';
+import { MortgageCalculatorComponent } from './components/mortgage-calculator/mortgage-calculator.component';
+import { DocumentViewerComponent } from './components/document-viewer/document-viewer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LandingPageComponent,
+    DocumentsPageComponent,
+    AuthPageComponent,
+    LoadingComponent,
+    ErrorComponent,
+    MortgageCalculatorComponent,
+    DocumentViewerComponent,
+    HeaderComponent,
+    DocumentsComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
