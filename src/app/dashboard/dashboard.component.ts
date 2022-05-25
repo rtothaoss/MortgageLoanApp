6fd72586a93094c891d87d88d1ab92b8bb525901
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-25 18:50:46 -0500

    completed styling so far on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
+import { faDollar, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +10,7 @@ import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons';
 export class DashboardComponent implements OnInit {
 
   arrowRight = faArrowAltCircleRight;
+  faDollar = faDollarSign;
 
   constructor() { }
 
